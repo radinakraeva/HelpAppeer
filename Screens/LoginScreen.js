cf4897b80fc244dabd77fd8bc973b274b9882710
arthur <arthur.low.2018@uni.strath.ac.uk>
2021-03-02 11:11:29 +0000

    Image folder added with Login Image

---
+++ Screens/LoginScreen.js
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View,Image,Text,Button,StyleSheet} from 'react-native';
+
+const LoginScreen = () => {
+
+    return (
+        <View>
+            <Image source={}/>
+        </View>
+    );
+};
+
+
+
+
+export default LoginScreen;
