15f7509dcd1331b1268aa0e99725faa5b107ff4d
Oxbeefbabe <44934519+Oxbeefbabe@users.noreply.github.com>
2021-03-03 16:46:36 +0000

    Created Chat List Screen, started on Chat Screen

--- App.js
+++ App.js
@@ -4,14 +4,28 @@ import { StyleSheet, View, ScrollView,Text} from 'react-native';
 
 import FeedScreen from './Screens/FeedScreen';
 import CircleIcon from './Components/CircleIcon';
-
+import ChatListScreen from './Screens/ChatListScreen';
+import ChatScreen from './Screens/ChatScreen';
 
 const App = () => {
-  return (
-      <View style = {{backgroundColor: '#fafdf3', padding: 20}}>
-              <FeedScreen/>
-      </View>
-  );
+
+    /*return (
+        <View style = {{backgroundColor: '#fafdf3', padding: 20}}>
+            <ChatListScreen/>
+        </View>
+    );*/
+
+    /*return (
+        <View style = {{backgroundColor: '#fafdf3', padding: 20}}>
+            <FeedScreen/>
+        </View>
+    );*/
+
+    return (
+        <View style = {{backgroundColor: '#fafdf3', padding: 20}}>
+            <ChatScreen listingName = "Aldi Please"/>
+        </View>
+    );
 };
 
 export default App;
---
+++ Components/ChatTextBox.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+
+import ColourPalette from '../Resources/ColourPalette';
+import CircleIcon from '../Components/CircleIcon';
+
+export default function ChatTextBox(){
+    return (
+        <View>
+
+        </View>
+    );
+}
