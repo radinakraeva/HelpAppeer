94507221af9fb316ed819311b92c793835f290b7
arthur <arthur.low.2018@uni.strath.ac.uk>
2021-03-03 19:25:23 +0000

    Login Screen design completed to match agreed styles, the password icon can be clicked to switch the image and remove the protection on the password so it can be read. the login button is not yet connected to the subsequent pages, I have a good idea of how to create a registration page if required (just need to check how we want to store the data and if we want to go as far as not allowing repeated usernames). added forgotten password but again the functionality can be applied once we confirm if its required or an unnessasery extra.

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,17 +1,129 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {View,Image,Text,Button,StyleSheet} from 'react-native';
+import {View,Image,TextInput,StyleSheet,Dimensions,TouchableOpacity,Text } from 'react-native';
 
-const LoginScreen = () => {
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import ColourPalette from '../Resources/ColourPalette';
+
+const LoginScreen = (signUp)=> {
+
+    const [data, setData] = React.useState({
+        password: '',
+        check_text: false,
+        secureTextEntry: true,
+    });
+
+    const passwordChange = (input) => {
+            setData({
+               ...data,
+               password: input,
+               check_text:true,
+            });
+        };
+
+    const update = () => {
+        setData({
+            ...data,
+            secureTextEntry: !data.secureTextEntry,
+        });
+    };
 
     return (
-        <View>
-            <Image source={}/>
+        <View style={styles.backing}>
+            <View style={styles.alin}>
+                <Image style={styles.pic} source={require('../Resources/Images/SignIn.png')}/>
+                <View style={styles.input} >
+                    <TextInput style={styles.textInput} placeholder="Username"/>
+                    <FontAwesome5 style={styles.icon} name="user-circle" size={30}/>
+                </View>
+                <View style={styles.input} >
+                    <TextInput style={styles.textInput} placeholder="Password" secureTextEntry={data.secureTextEntry} onChangeText={(input)=> passwordChange(input)}/>
+                    <TouchableOpacity onPress={update}>
+                        {data.secureTextEntry ?
+                            <Feather style={styles.icon2} name='eye-off' size={30}/>
+                            :
+                            <Feather style={styles.icon} name='eye' size={30}/>
+                        }
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity style={styles.alin} onPress= {() => signUp.navigate('SignUp')}>
+                        <Text style={{fontSize: 15, paddingTop:5, color:'gray' }}>Forgotten Password?</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.button}>
+                    <TouchableOpacity style={styles.alin}>
+                        <Text style={{fontSize: 30, paddingTop:15, fontWeight:'bold', color:'white'}}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity style={styles.alin}>
+                        <Text style={{fontSize: 20, paddingTop:15, fontWeight:'bold', color: ColourPalette.yellow,}}>New User? Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
         </View>
+
+
+
     );
-};
+}
 
+const {height} = Dimensions.get("screen");
+const picHeight = height*0.33;
 
+const styles = StyleSheet.create({
+    backing: {
+        backgroundColor: 'white',
+        flex: 1,
+    },
+    pic: {
+        width: picHeight,
+        height: picHeight,
+    },
+    alin: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    icon: {
+        flexDirection: 'row',
+        paddingLeft: 210,
+        paddingTop: 20,
+        color: ColourPalette.yellow,
+    },
+    icon2: {
+        flexDirection: 'row',
+        paddingLeft: 202,
+        paddingTop: 20,
+        color: ColourPalette.yellow,
+    },
+    input: {
+        flexDirection: 'row',
+        width: 350,
+        height: 80,
+        borderRadius: 20,
+        paddingLeft: 10,
+        marginTop: 20,
+        borderWidth: 3,
+        fontSize: 15,
+        borderColor: ColourPalette.yellow,
+    },
+    textInput: {
+        fontSize: 15,
+    },
+    button: {
+        marginTop: 25,
+        marginBottom: 25,
+        borderRadius: 20,
+        borderWidth: 3,
+        width: 150,
+        height: 80,
+        borderColor: ColourPalette.yellow,
+        backgroundColor: ColourPalette.yellow,
+    },
 
+});
 
 export default LoginScreen;
