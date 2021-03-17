5f5ebec700f9e0e423cf706edbd7b2546adf527e
Oxbeefbabe <44934519+Oxbeefbabe@users.noreply.github.com>
2021-03-17 23:09:56 +0000

    Message Screen styled

--- client/Components/ChatMessage.js
+++ client/Components/ChatMessage.js
@@ -1,14 +1,68 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import ColourPalette from '../Resources/ColourPalette';
 
 export default function ChatMessage({message, timeSent, userSent}){
+    let styles = StyleSheet.create();
     if(userSent === true){
-
+        styles = StyleSheet.create({
+            messageBox: {
+                backgroundColor: ColourPalette.darkBlue,
+                borderRadius: 10,
+                maxWidth: '60%',
+                margin: 5,
+                padding: 4,
+                display: 'flex',
+                alignSelf: 'flex-end',
+                justifyContent: 'flex-end',
+            },
+            text:{
+                color: ColourPalette.grey,
+                display: 'flex',
+                alignSelf: 'flex-end',
+                flexDirection: 'row-reverse',
+            },
+            time:{
+                color: ColourPalette.grey,
+                fontSize: 10,
+                display: 'flex',
+                alignSelf: 'flex-end',
+                flexDirection: 'row-reverse',
+            }
+        })
     }
+    else{
+        styles = StyleSheet.create({
+            messageBox: {
+                backgroundColor: ColourPalette.grey,
+                borderRadius: 10,
+                maxWidth: '60%',
+                margin: 5,
+                padding: 4,
+                display: 'flex',
+                alignSelf: 'flex-start',
+                justifyContent: 'flex-start',
+            },
+            text:{
+                color: '#000',
+                display: 'flex',
+                alignSelf: 'flex-start',
+                flexDirection: 'row',
+            },
+            time:{
+                color: '#000',
+                fontSize: 10,
+                display: 'flex',
+                alignSelf: 'flex-start',
+                flexDirection: 'row',
+            }
+        })
+    }
+
     return (
-        <View>
-            <Text>{message}</Text>
-            <Text>{timeSent}</Text>
+        <View style = {styles.messageBox}>
+            <Text style = {styles.text}>{message}</Text>
+            <Text style = {styles.time}>{timeSent}</Text>
         </View>
     );
 }
--- client/Screens/ChatScreen.js
+++ client/Screens/ChatScreen.js
@@ -1,38 +1,19 @@
 import React from 'react';
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet,TextInput} from 'react-native';
 
 import ColourPalette from '../Resources/ColourPalette';
 import CircleIcon from '../Components/CircleIcon';
 import ChatMessages from '../Components/ChatMessages';
-import ChatTextBox from "../Components/ChatTextBox";
-
-export default function ChatScreen({listingName}){
-    return (
-        <SafeAreaView style = {styles.chatScreen}>
-            <View style = {styles.topSection}>
-                <View style = {styles.topLeftSection}>
-                    <Text style = {styles.text}>Chat regarding:</Text>
-                    <Text style = {styles.titleText}>{listingName}</Text>
-                </View>
-            </View>
-            <View>
-                <ChatMessages/>
-                <View>
-                    <ChatTextBox/>
-                    <CircleIcon iconName='right' iconColor = {ColourPalette.darkBlue} size ={43} />
-                </View>
-            </View>
-        </SafeAreaView>
-    );
-}
 
 const styles = StyleSheet.create({
     chatScreen: {
         height: '100%',
+        display: 'flex',
     },
     topSection: {
         height: '15%',
         flexDirection: 'row',
+        padding: 10,
     },
     topLeftSection:{
         flex:1,
@@ -45,4 +26,47 @@ const styles = StyleSheet.create({
     text:{
         fontSize: 15,
     },
+    bottomSection:{
+        height: '10%',
+        minHeight: 60,
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 5,
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    textInput:{
+        backgroundColor: ColourPalette.grey,
+        borderRadius: 10,
+        padding: 3,
+        width: '80%',
+        height: '80%',
+    },
+    messages:{
+        display: 'flex',
+        height: '70%',
+
+    }
 })
+
+export default function ChatScreen({listingName}){
+    return (
+        <SafeAreaView style = {styles.chatScreen}>
+            <View style = {styles.topSection}>
+                <View style = {styles.topLeftSection}>
+                    <Text style = {styles.text}>Chat regarding:</Text>
+                    <Text style = {styles.titleText}>{listingName}</Text>
+                </View>
+            </View>
+            <View>
+                <ChatMessages style = {styles.messages}/>
+                <View style = {styles.bottomSection}>
+                    <TextInput style = {styles.textInput} placeholder = "Type your message here..."/>
+                    <CircleIcon iconName='right' iconColor = {ColourPalette.darkBlue} size ={43} />
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+
