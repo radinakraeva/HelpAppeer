f10a3d13af692ea56b69c578eaa9105182dacef4
Alina Varkki <alina.varkki.2018@uni.strath.ac.uk>
2021-03-24 20:26:09 +0200

    added card screen

--- client/Screens/CardPaymentScreen.js
+++ client/Screens/CardPaymentScreen.js
@@ -1,64 +1,31 @@
-import React from 'react'
-import { FormProvider, useForm } from 'react-hook-form'
-import {Alert, StyleSheet, KeyboardAvoidingView, Platform, SafeAreaView} from 'react-native'
-import LottieView from 'lottie-react-native'
-import CreditCardForm, { Button, FormModel } from 'rn-credit-card'
+import React, {useEffect} from 'react';
+import {SafeAreaView, View, TextInput, StyleSheet, TouchableOpacity, Text,} from 'react-native';
+import {CreditCardInput} from "react-native-payment-card";
 
-const CardPaymentScreen: React.FC = () => {
-    const formMethods = useForm<FormModel>({
-        mode: 'onBlur',
-        defaultValues: {
-            holderName: '',
-            cardNumber: '',
-            expiration: '',
-            cvv: '',
-        },
-    })
-    const { handleSubmit} = formMethods
 
-    function onSubmit(model: FormModel) {
-        Alert.alert('Success: ' + JSON.stringify(model, null, 2))
-    }
+
+const CardPaymentScreen = (props) => {
+
+
 
     return (
-        <FormProvider {...formMethods}>
-            <SafeAreaView style={styles.container}>
-                <KeyboardAvoidingView
-                    style={styles.avoider}
-                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-                >
-                    <CreditCardForm
-                        LottieView={LottieView}
-                        horizontalStart
-                        overrides={{
-                            labelText: {
-                                marginTop: 16,
-                            },
-                        }}
-                    />
-                </KeyboardAvoidingView>
-                    <Button
-                        style={styles.button}
-                        title={'CONFIRM PAYMENT'}
-                        onPress={handleSubmit(onSubmit)}
-                    />
-            </SafeAreaView>
-        </FormProvider>
-    )
+        <SafeAreaView style = {{paddingTop: 60}}>
+
+            <CreditCardInput
+                autoFocus
+                requiresName
+                requiresCVC
+                // labelStyle={}
+                // inputStyle={}
+                validColor={"black"}
+                invalidColor={"red"}
+                placeholderColor={"darkgray"}
+                // onChange={this._onChange}
+            />
+        </SafeAreaView>
+
+    );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    avoider: {
-        flex: 1,
-        padding: 36,
-    },
-    button: {
-        margin: 36,
-        marginTop: 0,
-    },
-})
 
-export default CardPaymentScreen
+export default CardPaymentScreen;
